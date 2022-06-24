e36652788357b5d8868a004e600d9ed8a02be32c
Bimla Gadtaula <bimlagd@gmail.com>
2022-06-24 16:29:57 +0545

    initial commit

---
+++ Controllers/AuthContoller.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssignmentNCC.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ViewResult Signup()
+        {
+            return View();
+        }
+        public ViewResult AdminLogin()
+        {
+            return View();
+        }
+        public ViewResult AdminDashboard()
+        {
+            return View();
+        }
+
+
+        public ViewResult Homepage()
+        {
+            return View();
+        }
+
+        public ViewResult SearchStudent()
+        {
+            return View();
+        }
+
+        public ViewResult AddStudent()
+        {
+            return View();
+        }
+
+        public ViewResult Delete()
+        {
+            return View();
+        }
+
+        public ViewResult StudentLogin()
+        {
+            return View();
+        }
+
+        public ViewResult StudentDashboard()
+        {
+            return View();
+        }
+
+    }
+}
+
